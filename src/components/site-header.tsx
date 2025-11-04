d4727e70e44e18a1abfc60f9b734c6c12485e5a4
Cauan Guerreiro <cauandevweb@gmail.com>
2025-11-04 15:50:16 +0000

    o texto ao lado da logo.img deve ser SONGNATION

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -10,9 +10,9 @@ export function SiteHeader() {
       <div className="container mx-auto flex h-20 items-center px-4 sm:px-6 lg:px-8">
         <div className="flex gap-6 md:gap-10">
           <Link href="/" className="flex items-center space-x-2">
-            <Image src={logo} alt="SongRise Logo" width={40} height={40} className="h-10 w-10" />
+            <Image src={logo} alt="SONGNATION Logo" width={40} height={40} className="h-10 w-10" />
             <span className="font-logo text-3xl font-bold inline-block text-foreground">
-              SongRise
+              SONGNATION
             </span>
           </Link>
         </div>
