dc2872613a87559e41d21b34e46f2810dbf3a3e0
Cauan Guerreiro <cauandevweb@gmail.com>
2025-11-04 15:11:11 +0000

    EU TENHO A LOGO

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -13,6 +13,7 @@ export const Logo = (props: SVGProps<SVGSVGElement>) => (
       strokeLinejoin="round"
       {...props}
     >
+      {/* Paste your SVG code here */}
       <path d="M9 18V5l12-2v13"/>
       <circle cx="6" cy="18" r="3"/>
       <circle cx="18" cy="16" r="3"/>
