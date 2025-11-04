f2bca66953bcd2b62b7c39126540d47bc43e81f3
Cauan Guerreiro <cauandevweb@gmail.com>
2025-11-04 22:28:58 +0000

    favicon, icon, and apple-icon The favicon, icon, or apple-icon file conv

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -2,7 +2,6 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import logo from '@/components/logo.png';
 import { Button } from "./ui/button";
 
 export function SiteHeader() {
@@ -11,7 +10,7 @@ export function SiteHeader() {
       <div className="container mx-auto flex h-20 items-center justify-between px-4 sm:px-6 lg:px-8">
         <div className="flex gap-6 md:gap-10">
           <Link href="/" className="flex items-center space-x-2">
-            <Image src={logo} alt="SONGNATION Logo" width={40} height={40} className="h-10 w-10" />
+            <Image src="/logo.png" alt="SONGNATION Logo" width={40} height={40} className="h-10 w-10" />
             <span className="font-logo text-3xl font-bold inline-block text-foreground">
               SONGNATION
             </span>
